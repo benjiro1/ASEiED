efa857cb9ae3f6d0e8624d9fa434bbb495d64b71
da_wid <da_wid@onet.pl>
2017-06-26 22:08:52 +0200

    Selection sort

--- HirszPeplinskiSobczak_0c1f71818d1144308c8e28fc56136654/src/main/scala/com/jwszol/SortJob.scala
+++ HirszPeplinskiSobczak_0c1f71818d1144308c8e28fc56136654/src/main/scala/com/jwszol/SortJob.scala
@@ -21,14 +21,34 @@ class SortJob {
     Logger.getLogger("akka").setLevel(Level.OFF)
 
     val jsonRDD = sparkSession.sparkContext.wholeTextFiles("./src/main/resources/dataMay-31-2017.json").map(x => x._2)
+    //jsonRDD.collect().foreach(println)
     val readedJsonRDD = sparkSession.read.json(jsonRDD)
-
+    readedJsonRDD.collect().foreach(println)
+    //val parsedArray = readedJsonRDD.map(x => x._2)
     val explodeJson = readedJsonRDD.withColumn("data", explode(readedJsonRDD.col("data"))).select("data")
     explodeJson.show()
     explodeJson.createOrReplaceTempView("tab")
+    
+    val values_with_ids = sparkSession.sql("SELECT cast(data[0] as integer) as id, cast(data[1] as float) as value FROM tab")
+    values_with_ids.createOrReplaceTempView("source_table")
+    //values_with_ids.collect().foreach(println)
+    val len = values_with_ids.count().toInt
+    //val arr3 = arr2.collect()
+    val min_val_row = sparkSession.sql("SELECT * FROM source_table WHERE value=(SELECT MIN(value) FROM source_table)").collect()(0)
 
-    sparkSession.sql("SELECT cast(data[0] as integer) as id, cast(data[1] as float) as value FROM tab").show()
-
+    var sorted_list : List[(Any,Any)] = List()
+    sorted_list = sorted_list:+((min_val_row(0),min_val_row(1)))    
+    
+    //sparkSession.sql("SELECT * FROM source_table ORDER BY value").collect.foreach(println)
+    val t0 = System.currentTimeMillis()
+    for(i <- 2 until (len + 1)) {
+      val curr_row = sparkSession.sql("SELECT * FROM source_table ORDER BY value").collect()(i-1)
+      sorted_list = sorted_list:+((curr_row(0),curr_row(1)))
+    }
+    sorted_list.foreach(println)
+    
+    val t1 = System.currentTimeMillis()
+    println("Selection sort time: " + (t1 - t0) + " ms")
   }
 
 }
